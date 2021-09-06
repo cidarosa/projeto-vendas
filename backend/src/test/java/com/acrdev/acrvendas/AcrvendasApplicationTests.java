dbee861842d489e30778d6bbc3152d859e208bc8
Aparecida Rosa <lucita.rosa@gmail.com>
2021-09-06 15:16:53 -0300

    Project created

---
+++ backend/src/test/java/com/acrdev/acrvendas/AcrvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acrdev.acrvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcrvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
